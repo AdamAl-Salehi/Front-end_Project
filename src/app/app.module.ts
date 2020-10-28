da2323a37c7f1901b67651e511d2475fb1eee0ed
AdamAl-Salehi <email@example.combakr.arkan@icloud.com>
2020-10-28 09:22:38 +0100

    last section of landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RobotsAndAiComponent } from './robots-and-ai/robots-and-ai.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RobotsAndAiComponent
   ],
   imports: [
     BrowserModule,
