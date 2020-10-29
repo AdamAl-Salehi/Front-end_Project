95b9c955422ce017f868c70617e872007f9e93f0
AdamAl-Salehi <email@example.combakr.arkan@icloud.com>
2020-10-29 09:11:04 +0100

    added bubble and started the news blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TestimonialComponent } from './testimonial/testimonial.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeroComponent } from './hero/hero.component';
+import { NewsBlogComponent } from './news-blog/news-blog.component';
 
 
 
@@ -31,7 +32,8 @@ import { HeroComponent } from './hero/hero.component';
     TestimonialComponent,
     HeaderComponent,
     FooterComponent,
-    HeroComponent
+    HeroComponent,
+    NewsBlogComponent
 
   ],
   imports: [
